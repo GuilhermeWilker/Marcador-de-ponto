5de209c8e2334ca7771cd7f40d9d190ab08daf0f
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-08-05 15:01:30 -0300

    feat: PWA estrutura

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("sw.js")
+      .then((registration) => {
+        console.log(
+          "Service Worker registrado com sucesso:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error("Falha ao registrar Service Worker:", error);
+      });
+  });
+}
+
 let pontos = [];
 let pontoDeEntrada = true;
 let ultimoPontoAberto = false;
---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "registro-de-pontos-cache-v1";
+const urlsToCache = ["/", "index.html", "script.js", "icon.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
